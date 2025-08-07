954213dc8f4909b54b9e5bb51f11c5ff98b12339
raissa-eugenio <eugenio.r@aluno.ifsp.edu.br>
2025-08-07 17:23:23 -0300

    começando o projeto

---
+++ src/components/Topo/index.js
@@ -0,0 +1,12 @@
+export default function Topo() {
+    return(
+        <header>
+            <div>
+                <picture>
+                    <a href="/"><img src=''></img></a>
+                </picture>
+            </div>
+        </header>
+    )
+
+}
